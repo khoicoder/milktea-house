3ad904aa1029a5b101f66287d806fceadcc67c81
Duc Luan <ducluan2310@gmail.com>
2026-03-15 00:38:33 +0700

    them thong tin sp

--- pages/product_detail.php
+++ pages/product_detail.php
@@ -26,7 +26,7 @@ exit();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="../css/style1.css">
+<link rel="stylesheet" href="../css/style_product-detail.css">
 
 <title><?php echo $product['name']; ?></title>
 
