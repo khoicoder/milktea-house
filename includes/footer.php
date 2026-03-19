9067cdde48dedb5596a35375d53261372ccc0d4d
lmkhoi <nq5494150@gmail.com>
2026-03-19 20:12:24 +0700

    Khoi - check

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <footer>
 
 <p>© 2026 MilkTea House – Online Milk Tea Ordering Website</p>
+<p>Follow us on social media!</p>
+<p> <a href="https://www.facebook.com/ma.progame101" target="_blank">Facebook</a> | <a href="https://www.instagram.com/" target="_blank">Instagram</a> | <a href="https://twitter.com/" target="_blank">Twitter</a></p>
 
 
 <p>Contact: <a href="mailto: 5aemcafe@gmail.com">5aemcafe@gmail.com</a>  Liên hệ : 0123456789</p>
